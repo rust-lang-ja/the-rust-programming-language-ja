85655a7b899c564163f3733bdbf7a68ccea19db9
Tatsuya Kawano <tatsuya@hibaridb.org>
2016-06-04 20:52:09 +0800

    Run CircleCI-based CI process on PR (Pull Request)
    - Add circle.yml and tools/circleci/setup-rust.sh
    - Update Makefile to take an optional VERSION string so that CI can
      generate HTML for both Rust 1.9 and 1.6 docs.
    - Update CONTRIBUTING.md and README.md with brief CI info.
    - Add public/1.9/index.html so that initial CI run for 1.9 will not fail.

---
+++ circleci/setup-rust.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -e
+set -u
+
+RUST_HOME=$1
+RUST_NIGHTLY_RELEASE_DATE=$2
+VERSION="nightly-$RUST_NIGHTLY_RELEASE_DATE"
+
+if [ -d $RUST_HOME ]; then
+  echo "Using cached Rust version $VERSION at $RUST_HOME"
+else
+  echo "Installing Rust version $VERSION to $RUST_HOME using rustup.sh"
+  curl -sSf https://static.rust-lang.org/rustup.sh | \
+      sh -s -- --prefix=$RUST_HOME --channel=nightly --$RUST_NIGHTLY_RELEASE_DATE --disable-sudo
+fi
