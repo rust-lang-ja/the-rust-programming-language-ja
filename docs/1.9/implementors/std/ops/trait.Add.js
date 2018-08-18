99d3f1d9e511f3db6277d3abc287f17f97f1fa0d
bn-e (CircleCI) <bn-e@hibaridb.org>
2018-08-18 01:13:32 +0000

    ci: generate pages at ba98eec [ci skip]

---
+++ docs/1.9/implementors/std/ops/trait.Add.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors['std'] = ["impl&lt;'a&gt; <a class='trait' href='std/ops/trait.Add.html' title='std::ops::Add'>Add</a>&lt;&amp;'a <a class='primitive' href='primitive.str.html'>str</a>&gt; for <a class='struct' href='std/string/struct.String.html' title='std::string::String'>String</a>","impl <a class='trait' href='std/ops/trait.Add.html' title='std::ops::Add'>Add</a> for <a class='struct' href='std/time/struct.Duration.html' title='std::time::Duration'>Duration</a>","impl <a class='trait' href='std/ops/trait.Add.html' title='std::ops::Add'>Add</a>&lt;<a class='struct' href='std/time/struct.Duration.html' title='std::time::Duration'>Duration</a>&gt; for <a class='struct' href='std/time/struct.Instant.html' title='std::time::Instant'>Instant</a>","impl <a class='trait' href='std/ops/trait.Add.html' title='std::ops::Add'>Add</a>&lt;<a class='struct' href='std/time/struct.Duration.html' title='std::time::Duration'>Duration</a>&gt; for <a class='struct' href='std/time/struct.SystemTime.html' title='std::time::SystemTime'>SystemTime</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
---
+++ docs/1.9/std/io/error/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"enum":[["ErrorKind","A list specifying general categories of I/O error."]],"struct":[["Error","The error type for I/O operations of the `Read`, `Write`, `Seek`, and associated traits."]],"type":[["Result","A specialized `Result` type for I/O operations."]]});
